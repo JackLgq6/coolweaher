21095ca556b5ad4a9a0bbcd0e20c16775ab2cc0e
Jack <719299302@qq.com>
2017-07-10 14:30:10 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/eg/coolweather/fragment/ChooseAreaFragment.java
+++ app/src/main/java/com/eg/coolweather/fragment/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.eg.coolweather.fragment;
 
 import com.eg.coolweather.R;
+import com.eg.coolweather.activity.MainActivity;
 import com.eg.coolweather.activity.WeatherActivity;
 import com.eg.coolweather.db.City;
 import com.eg.coolweather.db.County;
@@ -100,10 +101,17 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity weatherActivity = (WeatherActivity) getActivity();
+                        weatherActivity.drawerLayout.closeDrawers();
+                        weatherActivity.swipeRefresh.setRefreshing(true);
+                        weatherActivity.requestWeather(weatherId);
+                    }
                 }
             }
         });
